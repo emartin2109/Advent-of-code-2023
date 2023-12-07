ad9cb15ac356256222c56c3a28288eaffd72dcc5
emartin2109 <114905452+emartin2109@users.noreply.github.com>
2023-12-07 12:24:47 +0100

    [ADD] day01

---
+++ day01/part2/main.py
@@ -0,0 +1,50 @@
+# Const values
+special_strings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+directory = "input/input.txt"
+
+
+# Parse the file so it become a list of lines
+def parse_file(dir):
+    return open(dir).read().split("\n")[:-1]
+
+
+# Set the calibration pair accordingly to the given number
+def get_new_calibration_pair(first_calibration, last_calibration, number):
+    if first_calibration is None:
+        first_calibration = int(number)
+        last_calibration = int(number)
+    else:
+        last_calibration = int(number)
+    return first_calibration, last_calibration
+
+
+# Get the calibration value hidden in the given line
+def get_calibration_in_line(line):
+    first_calibration = None
+    last_calibration = None
+
+    for i in range(len(line)):
+        character = line[i]
+
+        if character.isdigit():
+            first_calibration, last_calibration = get_new_calibration_pair(first_calibration, last_calibration, character)
+
+        for j in range(len(special_strings)):
+            if line[i:].find(special_strings[j]) == 0:
+                first_calibration, last_calibration = get_new_calibration_pair(first_calibration, last_calibration, j + 1)
+
+    return first_calibration * 10 + last_calibration
+
+
+# Resolve the day01-part2 problem
+def main():
+    result = 0
+    lines = parse_file(directory)
+
+    for line in lines:
+        result += get_calibration_in_line(line)
+
+    return result
+
+
+print(main())
